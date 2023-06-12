d60f48da3a6e8a61c7a04e69d9a983b76ed78283
Luis Carlos Pando <hey@luiscarlospando.com>
2023-06-12 17:20:57 -0600

    Agregar soporte para WooCommerce

--- functions.php
+++ functions.php
@@ -628,13 +628,24 @@ add_shortcode( 'product_reviews_shortcode', 'product_reviews_shortcode' );
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
-add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+// Add WooCommerce support to theme
+function vitalhealth_add_woocommerce_support() {
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 150,
+        'single_image_width'    => 300,
 
-function my_theme_wrapper_start() {
-    echo '<section id="main-tuuut">';
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 2,
+            'max_columns'     => 5,
+        ),
+    ) );
+
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
 }
 
-function my_theme_wrapper_end() {
-    echo '</section>';
-}
+add_action( 'after_setup_theme', 'vitalhealth_add_woocommerce_support' );
