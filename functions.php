ab0e99d26e4ce6eb33fdad06cf84be7232724f4e
Luis Carlos Pando <hey@luiscarlospando.com>
2023-06-12 18:05:14 -0600

    Quitar settings manuales de WooCommerce

--- functions.php
+++ functions.php
@@ -577,17 +577,7 @@ remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wra
 // Add WooCommerce support to theme
 function vitalhealth_add_woocommerce_support() {
     add_theme_support( 'woocommerce', array(
-        'thumbnail_image_width' => 150,
-        'single_image_width'    => 300,
-
-        'product_grid'          => array(
-            'default_rows'    => 3,
-            'min_rows'        => 2,
-            'max_rows'        => 8,
-            'default_columns' => 4,
-            'min_columns'     => 2,
-            'max_columns'     => 5,
-        ),
+        
     ) );
 
     add_theme_support( 'wc-product-gallery-lightbox' );
