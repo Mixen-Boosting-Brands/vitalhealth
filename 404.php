d6b178be20fda403e2610ece6609f69283326590
Luis Carlos Pando <hey@luiscarlospando.com>
2023-06-13 13:47:04 -0600

    Armar markup de página 404

--- 404.php
+++ 404.php
@@ -1,24 +1,20 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
-
-			<!-- article -->
-			<article id="post-404">
-
-				<h1><?php esc_html_e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo esc_url( home_url() ); ?>"><?php esc_html_e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+	<section class="interna py-60">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center mb-5">
+                    <h1 class="text-uppercase"><?php esc_html_e( 'Error 404: Página no encontrada', 'html5blank' ); ?></h1>
+                </div>
+			<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
+				<div class="col-12">
+					<p>La página solicitada no existe o cambió de ubicación. <a href="<?php echo esc_url( home_url() ); ?>"><?php esc_html_e( 'Regresar al inicio', 'html5blank' ); ?></a>.</p>
+				</div>
+			<?php endwhile; ?>
+			<?php else : ?>
+			<?php endif; ?>
+            </div>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
