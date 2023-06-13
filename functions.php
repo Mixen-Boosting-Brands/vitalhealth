d0bde41eabb33fc55551a1a6012e6b71750686d1
Luis Carlos Pando <hey@luiscarlospando.com>
2023-06-13 12:42:19 -0600

    Quitar clase 'nav-link'

--- functions.php
+++ functions.php
@@ -475,13 +475,11 @@ function my_header_add_to_cart_fragment( $fragments ) {
     ob_start();
     $count = WC()->cart->cart_contents_count;
     ?>
-    <li class="nav-item">
     <?php if ( $count > 0 ) { ?>
         <a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'Carrito' ); ?>">
             <i class="fas fa-shopping-cart"></i> <span class="d-none d-lg-inline-block">Carrito</span> (<span class="cart-contents-count"><?php echo esc_html( $count ); ?></span>)
         </a>
     <?php } ?>
-    </li>
     <?php
  
     $fragments['a.cart-contents'] = ob_get_clean();
