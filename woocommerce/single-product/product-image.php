56c7a47fd9512fec08bf8496d855d3f7138d243d
Luis Carlos Pando <hey@luiscarlospando.com>
2023-06-12 20:28:51 -0600

    Agregar markup personalizado a hook de imagen del producto

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -41,9 +41,9 @@ $wrapper_classes   = apply_filters(
 
 		<?php
 		if ( $post_thumbnail_id ) {
-			$html  = '<div class="item card"><div class="woocommerce-product-gallery__image--placeholder thumbnail">';
+			$html  = '<div class="item card"><div class="woocommerce-product-gallery__image--placeholder thumbnail"><div class="card-body">';
 			$html .= wc_get_gallery_image_html( $post_thumbnail_id, true );
-			$html .= '</div></div>';
+			$html .= '</div></div></div>';
 		} else {
 			$html  = '<div class="woocommerce-product-gallery__image--placeholder">';
 			$html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
