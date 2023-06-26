f5d49fad78e32fdd19d39f854bcb67e475315ff2
Luis Carlos Pando <hey@luiscarlospando.com>
2023-06-26 10:30:03 -0600

    Descachear hoja de estilos

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 
 	<?php wp_head(); ?>
 
-    <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/css/styles.css">
+    <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/css/styles.css?ver=<?php echo rand(111,999)?>">
 </head>
 <body>
     <div id="backdrop"></div>
