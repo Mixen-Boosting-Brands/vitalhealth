c245e692955f62b05085ca30455b121bb9f7c9fb
Luis Carlos Pando <hey@luiscarlospando.com>
2023-10-03 13:33:44 -0600

    Agregar comentarios a código de modales

--- src/modals.js
+++ src/modals.js
@@ -1,6 +1,7 @@
 const modalContenidoVariable = document.getElementById('modal-contenido-variable');
 
 if (modalContenidoVariable) {
+    // Actualizar dinámicamente video de YouTube
     modalContenidoVariable.addEventListener('show.bs.modal', event => {
         // Button that triggered the modal
         const button = event.relatedTarget
@@ -15,6 +16,7 @@ if (modalContenidoVariable) {
         iframeYoutube.src = `https://www.youtube.com/embed/${videoId}`;
     });
 
+    // Autostop al cerrar el modal
     modalContenidoVariable.addEventListener('hidden.bs.modal', event => {
         // Update the modal's content.
         const iframeYoutube = modalContenidoVariable.querySelector('.iframe-youtube');
