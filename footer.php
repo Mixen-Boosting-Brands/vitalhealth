647ba12866ca849959067037fd467e4e0f11afdd
Luis Carlos Pando <hey@luiscarlospando.com>
2023-10-03 13:09:59 -0600

    Agregar botón de cerrar a modal

--- footer.php
+++ footer.php
@@ -136,6 +136,9 @@
 
     <div class="modal fade" id="modal-contenido-variable" tabindex="-1" aria-labelledby="modal-contenido-variableLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
             <div class="modal-content">
                 <div class="modal-body">
                     <iframe class="iframe-youtube" width="100%" height="615" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
