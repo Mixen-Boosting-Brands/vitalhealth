abecdd2e8186a8dbb1531d789ca9fd9f18503211
Luis Carlos Pando <hey@luiscarlospando.com>
2023-10-03 13:19:16 -0600

    Agregar autoplay a Testimonios

--- footer.php
+++ footer.php
@@ -142,7 +142,7 @@
                 </div>
                 <div class="modal-body">
                     <div class="ratio ratio-16x9">
-                        <iframe class="iframe-youtube" width="100%" height="615" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        <iframe class="iframe-youtube" width="100%" height="615" src="" allow="autoplay;" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
